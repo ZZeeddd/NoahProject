831f66d22353be0496140ff7edfdae0d16fb56d9
noahkassin-admin <noah.kassin@feelbelt.de>
2022-08-15 16:38:19 +0200

    Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Magic'),
+      home: const MyHomePage(title: 'Magnificant'),
     );
   }
 }
